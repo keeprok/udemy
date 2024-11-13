3432668c49a78b0742773beca4b236c2ac13154f
keeprok <140785214+keeprok@users.noreply.github.com>
2024-11-13 18:08:42 +0900

    설정 추가

--- weather-dashboard/vite.config.ts
+++ weather-dashboard/vite.config.ts
@@ -1,12 +1,19 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 });
